920521c5b1924eb6550aaac80079a3fbaa840e4c
Alicia Garnier <105979924+aliciagarnier@users.noreply.github.com>
2023-09-23 09:53:15 -0300

    calculator implementation

---
+++ Calculator/toDoList/src/main/java/org/example/exception/DuplicatedChoreException.java
@@ -0,0 +1,17 @@
+package org.example.exception;
+
+public class DuplicatedChoreException extends RuntimeException {
+
+
+
+
+
+    public DuplicatedChoreException (String message) {
+
+        super(message);
+
+
+    }
+
+
+}
---
+++ Calculator/toDoList/src/main/java/org/example/exception/InvalidDeadlineException.java
@@ -0,0 +1,15 @@
+package org.example.exception;
+
+public class InvalidDeadlineException extends RuntimeException {
+
+
+
+    public InvalidDeadlineException (String message) {
+
+        super(message);
+    }
+
+
+
+
+}
